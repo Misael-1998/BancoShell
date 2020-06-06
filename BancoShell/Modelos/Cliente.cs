a25b4aa1caf5fe1c36c484ef51ddf5cdde2e8a50
Misael-1998 <misaelgg1998@hotmail.com>
2020-06-06 15:41:52 -0600

    Primer commit del bancoShell

---
+++ BancoShell/Modelos/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoShell.Modelos
+{
+    public class Cliente
+    {
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        public Cliente(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+
+
+
+    }
+}
---
+++ BancoShell/Program.cs
@@ -0,0 +1,27 @@
+﻿using BancoShell.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BancoShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cliente> clientes = new List<Cliente> { 
+            
+                new Cliente("Juan"),
+                new Cliente("Miguelito"),
+                new Cliente("Flor")
+            };
+
+            for (int i = 0; i < clientes.Count; i++)
+            {
+                Console.WriteLine(clientes[i].Nombre);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
