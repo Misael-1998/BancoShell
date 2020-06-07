f7995f70d9fab8540669d7dfcd8ac05dd375b671
Misael-1998 <misaelgg1998@hotmail.com>
2020-06-06 20:44:12 -0600

    segundo commit insercion de nombres de clientes

---
+++ BancoShell/Modelos/Mostrar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoShell.Modelos
+{
+    class Mostrar
+    {
+        ArrayList Lista;
+
+        public Mostrar()
+        {
+            Lista = new ArrayList();
+        }
+
+        public void CapturaDatos()
+        {
+            int cantidad;
+            string nombre;
+            Console.WriteLine("Ingrese la candtidad de nombres que decea");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Ingrese los nombres deseados");
+                nombre = Console.ReadLine();
+                Lista.Add(nombre);
+            }
+        }
+
+        public void ImprimirDatos()
+        {
+            foreach (var item in Lista)
+            {
+                Console.WriteLine("Estos son los nombres ingresados");
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+      
+    }
+}
